54644e2bd63fcc27ad082b2f4df0adb1a631edac
VanshDevChoudhary <vanshdevchoudhary007@gmail.com>
2025-03-24 23:15:28 +0530

    Adding Resource Allocation Branch

--- project.py
+++ project.py
@@ -4,3 +4,19 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+class ResourceAllocationSimulator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Resource Allocation Graph Simulator")
+        
+        self.graph = nx.DiGraph()
+        
+        self.canvas = tk.Canvas(self.root, width=600, height=400)
+        self.canvas.pack()
+        
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
+        
+        tk.Label(self.frame, text="Process/Resource Name:").grid(row=0, column=0)
+        self.node_entry = tk.Entry(self.frame)
+        self.node_entry.grid(row=0, column=1)
